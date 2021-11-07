6a76156b85cc5a769d0853f1f711547740d36782
Karina Lyapina <ivettra@yandex.ru>
2021-11-07 21:29:32 +0500

    feat: коммит перед ревью

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+// Порт
 const { PORT = 3001 } = process.env;
 
 const app = express();
